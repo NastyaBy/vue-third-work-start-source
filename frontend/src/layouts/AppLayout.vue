522d2b20f184df7cfcfbf71f7da6edabdadf6243
Anastasiya Gerasimenko <agerasimenko@pnblab.com>
2023-05-10 18:00:00 +0300

    2.11. Добавление бэклога на доску задач в проекте Vue-Work

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,9 +1,33 @@
 <template>
-  <div class="app_layout">
-    <app-layout-header />
-    <slot />
-  </div>
+    <div class="app_layout">
+        <app-layout-header/>
+        <div class="content">
+            <app-layout-sidebar
+                    :tasks="props.tasks"
+                    :filters="props.filters"
+                    @update-tasks="$emit('updateTasks', $event)"
+            />
+            <slot/>
+        </div>
 </template>
+
+<script setup>
+import AppLayoutHeader from './AppLayoutHeader.vue';
+import AppLayoutSidebar from './AppLayoutSidebar.vue';
+
+const props = defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  },
+  filters: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(['updateTasks']);
+</script>
 
 <style lang="scss" scoped>
 .app_layout {
@@ -11,8 +35,11 @@
   flex-direction: column;
   height: 100vh;
 }
+
+.content {
+  display: flex;
+  flex-grow: 1;
+}
 </style>
 
-<script setup>
-import AppLayoutHeader from "./AppLayoutHeader.vue";
-</script>
+
