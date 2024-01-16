cd01ef0ea03c46b495be4c092f4f556b7fd02c88
Nastya Gerasimenko <nastya.gerasimenko@mavs.tech>
2024-01-16 08:52:15 +0300

    5.6. Аутентификация в проекте Vue-Work

--- frontend/src/common/components/AppDrag.vue
+++ frontend/src/common/components/AppDrag.vue
@@ -30,3 +30,37 @@ function onDrag({ dataTransfer }) {
 </script>
 
 <style scoped lang="scss"></style>
+<template>
+  <!--  Блокируем перенос блоков если пользователь не вошел в систему-->
+  <div
+    :draggable="authStore.isAuthenticated"
+    @dragstart.self="onDrag"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { DATA_TRANSFER_PAYLOAD, MOVE } from "../constants";
+import { useAuthStore } from "@/stores";
+
+const authStore = useAuthStore();
+
+const props = defineProps({
+  transferData: {
+    type: Object,
+    required: true,
+  },
+});
+
+function onDrag({ dataTransfer }) {
+  dataTransfer.effectAllowed = MOVE;
+  dataTransfer.dropEffect = MOVE;
+  dataTransfer.setData(
+    DATA_TRANSFER_PAYLOAD,
+    JSON.stringify(props.transferData)
+  );
+}
+</script>
